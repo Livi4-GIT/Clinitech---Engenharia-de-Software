c60f8da6b7146bc29f501fa345ceeeae3ad8a963
Lucas <lucaszmasashi@gmail.com>
2025-10-13 21:59:28 -0300

    arrumado o app.js

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Usuario from "./home/Usuario";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Usuario></Usuario>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
